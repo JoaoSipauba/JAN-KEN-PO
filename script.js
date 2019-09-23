85a4485ddcc332f1e2d0e5953eccc6a73067a32b
JoaoSipauba <joaosipauba@hotmail.com>
2019-09-23 12:06:15 -0300

    adicionando escolhas de usuario

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-
-document.getElementById("papel").onclick = function () {
-    alert("clicquei no papel")
-}
+// pedra = 0; papel = 1, tesoura = 2
 
 document.getElementById("pedra").onclick = function () {
-    alert("clicquei na pedra")
+    console.log("cliquei na pedra");
+    juiz(0)
+}
+
+document.getElementById("papel").onclick = function () {
+    console.log("clicquei no papel");
+    juiz(1)
 }
 
 document.getElementById("tesoura").onclick = function(){        
-    alert("clicquei na tesoura")
+    console.log("cliquei na tesoura");
+    juiz(2)
 }
 
+function juiz(userChoice) {
+    let choices = ["pedra", "papel", "tesoura"]
+
+
+    console.log(choices[userChoice]);
+
+}
