14ed8199c0d1d1552ec19678a188e9c59827ada3
JoaoSipauba <joaosipauba@hotmail.com>
2019-09-23 11:43:56 -0300

    adicionando eventos de clique

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("funcionou");
+
+document.getElementById("papel").onclick = function () {
+    alert("clicquei no papel")
+}
+
+document.getElementById("pedra").onclick = function () {
+    alert("clicquei na pedra")
+}
+
+document.getElementById("tesoura").onclick = function(){        
+    alert("clicquei na tesoura")
+}
+
