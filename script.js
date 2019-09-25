f1075f65aaeac12bdaf15168daca65f54feb83f7
alexgit088 <allex.git088@gmail.com>
2019-09-25 09:41:37 -0300

    ajustando merge

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 // pedra = 0; papel = 1, tesoura = 2
 
 document.getElementById("pedra").onclick = function () {
-    console.log("cliquei na pedra");
-    juiz(0)
+    let user = choices[0];
+    let cpuChoice = sort()
+    console.log("cpuChoice:" + cpuChoice);
 }
 
 document.getElementById("papel").onclick = function () {
-    console.log("clicquei no papel");
-    juiz(1)
+    let user = choices[1];
+    let cpuChoice = sort()
+    console.log("cpuChoice:" + cpuChoice);
 }
 
 document.getElementById("tesoura").onclick = function(){        
-    console.log("cliquei na tesoura");
-    juiz(2)
+    let user = choices[2];
+    let cpuChoice = sort()
+    console.log("cpuChoice:" + cpuChoice);    
 }
 
 function juiz(userChoice) {
