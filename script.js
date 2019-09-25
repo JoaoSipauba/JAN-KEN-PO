e0b5da9f48599d8926f303e7fe1e141a93b3b093
JoaoSipauba <joaosipauba@hotmail.com>
2019-09-25 10:14:21 -0300

    adicionando a funtion 'juiz' para decidir o resultado do confronto

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-choices = ["pedra", "papel", "tesoura"]
+choices = ["papel", "pedra", "tesoura"]
 
 function sort(){
     let randomGet = Math.floor(Math.random() * choices.length);
@@ -6,20 +6,39 @@ function sort(){
     return cpuRandom
 }
 
-document.getElementById("pedra").onclick = function () {
-    let user = choices[0];
+document.getElementById("papel").onclick = function () {
+    let userChoice = choices[0];
     let cpuChoice = sort()
     console.log("cpuChoice:" + cpuChoice);
+    console.log("userChoice:" + userChoice);
+    juiz(userChoice,cpuChoice)
 }
 
-document.getElementById("papel").onclick = function () {
-    let user = choices[1];
+document.getElementById("pedra").onclick = function () {
+    let userChoice = choices[1];
     let cpuChoice = sort()
     console.log("cpuChoice:" + cpuChoice);
+    console.log("userChoice:" + userChoice);
+    juiz(userChoice,cpuChoice)
+    
 }
 
 document.getElementById("tesoura").onclick = function(){        
-    let user = choices[2];
+    let userChoice = choices[2];
     let cpuChoice = sort()
-    console.log("cpuChoice:" + cpuChoice);    
+    console.log("cpuChoice:" + cpuChoice); 
+    console.log("userChoice:" + userChoice);   
+    juiz(userChoice,cpuChoice)
+}
+
+function juiz(userChoice, cpuChoice){
+    if (userChoice === cpuChoice){
+        console.log("empate");        
+    }
+    if ((userChoice === "pedra" && cpuChoice === "papel") || (userChoice == "papel" && cpuChoice === "tesoura" ) ||(userChoice === "tesoura" && cpuChoice === "pedra")) {
+        console.log("cpu ganhou");        
+    }
+    if ((userChoice === "pedra" && cpuChoice === "tesoura") || (userChoice == "papel" && cpuChoice === "pedra" ) ||(userChoice === "tesoura" && cpuChoice === "papel")) {
+        console.log("user ganhou");        
+    }
 }
