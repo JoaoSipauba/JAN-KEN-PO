2c95050769f4af5abef74df06e6ede5a83bcf38d
alexgit088 <allex.git088@gmail.com>
2019-09-25 09:38:11 -0300

    Revert "adicionando randomizaçâo na escolha do computador"
    This reverts commit 9aef126bb658f8183f7bfd51a02b96fdf49d9446.

--- script.js
+++ script.js
@@ -1,34 +1,24 @@
 // pedra = 0; papel = 1, tesoura = 2
 
-choices = ["pedra", "papel", "tesoura"]
-
-function sort(){
-    let randomGet = Math.floor(Math.random() * choices.length);
-    let cpuRandom = choices[randomGet];
-    return cpuRandom
-}
-
 document.getElementById("pedra").onclick = function () {
-    let user = choices[0];
-//    console.log(user);
-    let cpuChoice = sort()
-    console.log("cpuChoice:" + cpuChoice);
+    console.log("cliquei na pedra");
+    juiz(0)
 }
 
 document.getElementById("papel").onclick = function () {
-    let user = choices[1];
-//    console.log(user);
-    let cpuChoice = sort()
-    console.log("cpuChoice:" + cpuChoice);
+    console.log("clicquei no papel");
+    juiz(1)
 }
 
 document.getElementById("tesoura").onclick = function(){        
-    let user = choices[2];
-//    console.log(user);
-    let cpuChoice = sort()
-    console.log("cpuChoice:" + cpuChoice);    
+    console.log("cliquei na tesoura");
+    juiz(2)
 }
 
-function juiz() {
-    
+function juiz(userChoice) {
+    let choices = ["pedra", "papel", "tesoura"]
+
+
+    console.log(choices[userChoice]);
+
 }
